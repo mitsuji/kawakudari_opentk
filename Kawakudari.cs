35a6f279b20cac5b7bd8e9c452d1796b4d886c7c
mitsuji <tkms@mitsuji.org>
2020-09-30 08:47:27 +0900

    mod: refactoring

--- Kawakudari.cs
+++ Kawakudari.cs
@@ -5,68 +5,71 @@ using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using IchigoJam;
 
-public class Kawakudari : GameWindow
+public class Kawakudari
 {
   static void Main()
   {
-    new Kawakudari().Run(60.0);
+    new Kawakudari();
   }
 
-  private Random rand;
   private Std15 std15;
-  private uint frame = 0;
-  private int x = 15;
-  private bool running = true;
+  private Random rand;
 
-  public Kawakudari()
-          : base (512, 384, GraphicsMode.Default, "kawakudari")
+  private uint frame;
+  private int x;
+  private bool running;
+
+  public Kawakudari ()
   {
-    this.KeyDown += window_KeyDown;
+    std15 = new Std15(512, 384, 32, 24);
     rand = new Random();
-    std15 = new Std15(512,384,32,24);
+    GameWindow win = new GameWindow (512, 384, GraphicsMode.Default, "kawakudari");
+    win.Load += OnLoad;
+    win.UpdateFrame+= OnUpdateFrame;
+    win.KeyDown += OnKeyDown;
+    win.RenderFrame+= OnRenderFrame;
+    win.Resize += OnResize;
+    win.Run(60.0);
   }
 
-  protected override void OnLoad(EventArgs e)
+  private void OnLoad (object sender, EventArgs e)
   {
-    GL.ClearColor(0.0f,0.0f,0.0f,1.0f); // Black
-    base.OnLoad(e);
+    frame = 0;
+    x = 15;
+    running = true;
   }
-
-  protected override void OnResize(EventArgs e)
-  {
-    GL.Viewport(0,0,Width,Height);
-    base.OnResize(e);
-  }
-
-  protected override void OnUpdateFrame(FrameEventArgs e)
+  
+  private void OnUpdateFrame (object sender, FrameEventArgs e)
   {
     if (!running) return;
-    if(frame % 5 == 0)
-    {
+    if (frame % 5 == 0) {
       std15.Locate(x,5);
       std15.Putc('0');
       std15.Locate(rand.Next(0,32),23);
       std15.Putc('*');
 
       std15.Scroll();
-      if(std15.Scr(x,5) != '\0') running = false;
+      if (std15.Scr(x,5) != '\0') running = false;
     }
     frame ++;
-    base.OnUpdateFrame(e);
   }
-
-  protected override void OnRenderFrame(FrameEventArgs e)
-  {
-    GL.Clear(ClearBufferMask.ColorBufferBit);
-    std15.PAppletDraw();
-    Context.SwapBuffers();       
-    base.OnRenderFrame(e);
-  }
-
-  void window_KeyDown(object sender, KeyboardKeyEventArgs e)
+  
+  private void OnKeyDown (object sender, KeyboardKeyEventArgs e)
   {
     if(e.Key == Key.Left)  x--;
     if(e.Key == Key.Right) x++;
+  }
+
+  private void OnRenderFrame (object sender, FrameEventArgs e)
+  {
+    std15.DrawScreen();
+    ((GameWindow)sender).Context.SwapBuffers();
+  }
+
+  private void OnResize (object sender, EventArgs e)
+  {
+    GameWindow win = (GameWindow)sender;
+    GL.Viewport(0,0,win.Width,win.Height);
   }
 
 }
